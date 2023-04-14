3e244f2087887128944c5101421191695555e5a0
aemlocal <aemlocal@ED49005994P030.educa.jcyl.es>
2023-04-14 12:55:44 +0200

    Clase nueva

--- PruebaGit/src/pruebaGit/Nueva.java
+++ PruebaGit/src/pruebaGit/Nueva.java
@@ -1,11 +1,10 @@
 package pruebaGit;
 
-public class PruebaGit {
+public class Nueva {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		
-		System.out.println("");
 	}
 
 }
